c9fe4fac78b1d855b30b0ec5be5484da9a632475
Maximiliano Báez González <mxbg.py@gmail.com>
2011-11-14 03:57:40 -0300

    Se añadieron comentarios

--- src/py/una/ia/problemas/TSP.java
+++ src/py/una/ia/problemas/TSP.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
  */
 
 package py.una.ia.problemas;
@@ -10,7 +22,7 @@ import py.una.ia.util.Solucion;
 /**
  * Esta clase es una representación del problema del TSP
  * 
- * @author mbaez
+ * @author Maximiliano Báez <mxbg.py@gmail.com>
  */
 public class TSP {
     private int ciudades;
